9d037425ebf6e79d2f1c769df8c36bbf69fe6855
Brett Chalupa <brettchalupa@gmail.com>
2023-04-18 08:41:18 -0400

    feat: level 3, icon, exports

--- main_menu.gd
+++ main_menu.gd
@@ -1,10 +1,22 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("MainMenu ready!")
-
+	$Options/StartButton.grab_focus()
+	
+	if !OS.has_feature("pc"):
+		$Options/FullscreenButton.hide()
+		$Options/QuitButton.hide()
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://level_1.tscn")
+
+func _on_fullscreen_button_pressed():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+func _on_quit_button_pressed():
+	get_tree().quit()
